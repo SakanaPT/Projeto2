baaf3083a854a36440fc3c4ba7d3f365df5b61cf
João Carvalhosa <carvalhosa623@gmail.com>
2024-06-18 16:02:33 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,3 +20,4 @@ const port = process.env.SERVER_PORT || 3000; // Buscar "SERVER_PORT" ao ficheir
 app.listen(port, () => {
   console.log(`Server open at: http://localhost:${port}`);
 });
+module.exports = app;
